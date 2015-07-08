a21b050972e59911414c3d1d518c3b3d0705ef3b
ali1k <hyperir@gmail.com>
2015-07-09 00:02:59 +0200

    fixed a bug

--- convertTopicsToRDF.js
+++ convertTopicsToRDF.js
@@ -1,6 +1,6 @@
 var fs = require ('fs');
 var _ = require("lodash");
-for(var c = 1;c < = 716; c++){
+for(var c = 1;c <= 716; c++){
     var input = fs.readFileSync('data/o' + c + '.json','utf8');
     var output = '';
     var currentObj = JSON.parse(input);
@@ -11,7 +11,7 @@ for(var c = 1;c < = 716; c++){
         desc = node.desc;
         sura = node.sura;
         aya = node.aya;
-        rn = Math.round(+new Date() / 1000);
+        rn = c + '-' + i;
         output = output + '<http://tanzil.net/#' + sura + ':' + aya + '> <http://rdfs.org/sioc/ns#topic> <http://tanzil.ld-r.org/' + rn + '> . ';
         output = output + '<http://tanzil.ld-r.org/' + rn + '> a <http://tanzil.ld-r.org/vocab/Topic>; <http://www.w3.org/2000/01/rdf-schema#label> "' + subtopic + '"; <http://purl.org/dc/terms/description> """' + desc + '""". ';
         output = output + '<http://vajehyab.com/dehkhoda/' + encodeURIComponent(topic) + '> a <http://tanzil.ld-r.org/vocab/Topic> ; <http://www.w3.org/2000/01/rdf-schema#label> "' + topic + '"; <http://purl.org/dc/terms/hasPart> <http://tanzil.ld-r.org/' + rn + '> . ';
