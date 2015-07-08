57c2699b128407aadc047d53e9e76cabdd7b833f
ali1k <hyperir@gmail.com>
2015-07-08 22:27:11 +0200

    added fa translation

--- convertTranslationToRDF.js
+++ convertTranslationToRDF.js
@@ -1,4 +1,9 @@
 var fs = require ('fs');
+var _ = require("lodash");
 var input = fs.readFileSync('./data/translation-fa.json','utf8');
 var output = '';
 var currentObj = JSON.parse(input);
+_.forEach(currentObj, function(node, i) {
+    output = output + '<http://tanzil.net/#' + node.sura + ':' + node.aya + '> <http://purl.org/linguistics/gold/translation> """' +  node.text + '""" . ';
+});
+console.log(output);
